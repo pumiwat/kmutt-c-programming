e38bcc542e3e180c4a424f94376d35ff986bfb59
pumiwat <st3555@roong-aroon.ac.th>
2020-01-21 09:55:27 +0700

    create 1

---
+++ week1/hi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+
+    printf("hello,~ word!!\n");
+    printf("hello, word!!\n");
+    printf("hello, word!!\r");
+    printf("hello, \tword!!");
+
+    return 0;
+}
